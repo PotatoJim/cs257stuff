879d6c4081dda8db38f702bf1f0287f712e5e9eb
root <root@JonathanLaptop>
2024-04-22 10:18:52 -0500

    more edits

--- pythonlab/createtables.py
+++ pythonlab/createtables.py
@@ -1,15 +1,3 @@
-CREATE TABLE cities (
-    city text,
-    state text,
-    pop real,
-    latitude real,
-    longitude real
-)
-CREATE TABLE states (
-    code text,
-    state text,
-    pop real
-)
 
 def makeTable1():
 
