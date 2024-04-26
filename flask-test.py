35fd1a7e2dfa5d26fb2e5fc670686e52ed6fe19d
PotatoJim <vilmjo54@gmail.com>
2024-04-26 10:28:07 -0500

    updated flask code

--- flask-test.py
+++ flask-test.py
@@ -44,7 +44,7 @@ def my_pop(abbrev):
     if pop == None:
         return "Invalid State Code"
 
-    return f'{pop[0]}'
+    return f'Population of {abbrev} is' + '<h1 style="color:Red">' + f'{pop[0]}' + '<h1>'
 
 
 
